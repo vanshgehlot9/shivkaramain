436a30e0b4488f918044558836f63c1f8c161ea8
Vansh Gehlot <vanshgehlot67@gmail.com>
2025-11-24 20:58:31 +0530

    update2.1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
+import { Suspense } from "react";
 import "./globals.css";
 import Grain from "@/components/Grain";
 import Preloader from "@/components/Preloader";
@@ -58,7 +59,9 @@ export default function RootLayout({
       <body
         className={`${outfit.variable} antialiased bg-black text-white`}
       >
-        <AnalyticsTracker />
+        <Suspense fallback={null}>
+          <AnalyticsTracker />
+        </Suspense>
         <Preloader />
         <Grain />
         <WhatsAppWidget />
